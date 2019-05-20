c02e948e898a1dffe8d676b859f7d53743c488d6
Faisal Iqbal <bsc.f14.37@gmail.com>
2019-05-20 12:49:31 +0500

    faisal cli

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# faisaliqbal_cli
+
+
+> The [Fasial Iqbal](https://www.linkedin.com/in/faisal-iqbal-540a52112/) CLI
+
+<!-- <img src="1.png" width="752"> -->
+
+
+## Usage
+
+Install Node.js, then:
+
+```
+$ npx usamaliaquat
+```
+
+
+## Built with
+
+- [ink](https://github.com/vadimdemedes/ink) - React for interactive command-line apps
+
+
+
+## License
+
+MIT © [Faisal Iqbal](https://www.linkedin.com/in/faisal-iqbal-540a52112/)
---
+++ source/ui.js
@@ -0,0 +1,72 @@
+'use strict';
+const path = require('path');
+const {h, Text} = require('ink');
+const SelectInput = require('ink-select-input');
+const opn = require('opn');
+
+
+const open = url => opn(url, {wait: false});
+
+const handleSelect = item => {
+	if (item.url) {
+		open(item.url);
+	}
+	if (item.action) {
+		item.action();
+	}
+	if(item.skypeAction){
+		console.log(item.skypeAction())
+	}
+};
+
+const items = [
+
+	{
+		label: 'Github',
+		url: 'https://github.com/faisalrajput'
+	},
+	{
+		label: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/faisal-iqbal-540a52112/'
+	},
+	// {
+	// 	label: 'facebook',
+	// 	url: 'https://www.facebook.com/usama.ali.0303506'
+	// },
+	{
+		label: 'Private Email',
+		url: 'mailto:bsc.f14.37@gmail.com'
+	},
+	{
+		label: 'Skype',
+		skypeAction(){
+			return 'My skype Id is  ==> live:inconnent12345'
+		}
+	},
+	// TODO: Add separator item here when https://github.com/vadimdemedes/ink-select-input/issues/4 is done
+	{
+		label: 'Quit',
+		action() {
+			process.exit(); // eslint-disable-line unicorn/no-process-exit
+		}
+	}
+];
+
+module.exports = () => (
+	<div>
+		<br/>
+		<div>
+			<Text>Hello, I'm Faisal Iqbal</Text>
+		</div>
+		<br/>
+		<div>
+			<Text>I'm full stack javaScript developer.</Text>
+		</div>
+		<br/>
+		<div>
+			<Text>wanna to learn more about me 💥</Text>
+		</div>
+		<br/>
+		<SelectInput items={items} onSelect={handleSelect}/>
+	</div>
+);
